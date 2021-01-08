46a08a03d5a4c0dee78353db4f17e3545d567699
Dmitry Gulin <gulid@amazon.com>
2021-01-08 16:31:29 -0500

    fix java fodler structure and add java and c# entries to gitignore

--- java-example-extension/run.sh
+++ java-example-extension/run.sh
@@ -8,21 +8,20 @@ LAMBDA_FUNCTION=$2
 # Build artifacts
 mvn clean install
 
-# Perform cleanup & create zip
-rm -Rf extensions/*.jar
-rm -Rf extensions/*.zip
-mv target/java-example-extension-1.0-SNAPSHOT.jar extensions
+# Create zip
 chmod +x extensions/java-example-extension
-cd extensions
-zip -r extension.zip .
+archive="extension.zip"
+if [ -f "$archive" ] ; then
+    rm "$archive"
+fi
+zip "$archive" -j target/java-example-extension-1.0-SNAPSHOT.jar
+zip "$archive" extensions/*
 
 # Push extension
-aws lambda publish-layer-version --layer-name "${EXTENSION_NAME}" --zip-file "fileb://extension.zip"
+aws lambda publish-layer-version --layer-name "${EXTENSION_NAME}" --zip-file "fileb://$archive"
 
 # Update Lambda function to the latest version of external pushed as Lambda layers
 aws lambda update-function-configuration \
   --function-name ${LAMBDA_FUNCTION} --layers $(aws lambda list-layer-versions --layer-name ${EXTENSION_NAME} \
     --max-items 1 --no-paginate --query 'LayerVersions[0].LayerVersionArn' \
     --output text)
-
-cd -
--- java-example-extension/zip.sh
+++ java-example-extension/zip.sh
@@ -3,12 +3,11 @@
 # Build artifacts
 mvn clean install
 
-# Perform cleanup & create zip
-rm -Rf extensions/*.jar
-rm -Rf extensions/*.zip
-mv target/java-example-extension-1.0-SNAPSHOT.jar extensions
+# Create zip
 chmod +x extensions/java-example-extension
-cd extensions
-zip -r extension.zip .
-mv extension.zip ../
-cd -
+archive="extension.zip"
+if [ -f "$archive" ] ; then
+    rm "$archive"
+fi
+zip "$archive" -j target/java-example-extension-1.0-SNAPSHOT.jar
+zip "$archive" extensions/*
