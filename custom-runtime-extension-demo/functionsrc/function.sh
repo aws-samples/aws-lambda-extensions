d6e40f50b1908c4eca3cad7060b2a4be0064e972
Julian Wood <jrwood@amazon.com>
2021-02-25 10:17:24 +0000

    reorder and add additional logging data to custom runtime demo

--- custom-runtime-extension-demo/functionsrc/function.sh
+++ custom-runtime-extension-demo/functionsrc/function.sh
@@ -1,9 +1,12 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
 
+echo "[function] Running init code outside handler"
+
 function handler () {
   EVENT_DATA=$1
-  echo "[function] Receiving invocation: '$EVENT_DATA'"
+  echo "[function] handler receiving invocation: '$EVENT_DATA'"
+  sleep 1
   RESPONSE="Echoing request: '$EVENT_DATA'"
   echo $RESPONSE
 }
